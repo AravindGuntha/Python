f93eae545d4fcd170054626156b0ec0f7eb23833
AravindGuntha <60147302+AravindGuntha@users.noreply.github.com>
2024-12-03 15:55:40 +0530

    Basics Code of python AG000
    This code has
    Taking user input and performing some operations.

---
+++ UseBasics.py
@@ -0,0 +1,22 @@
+#take two user inputs and add those numbers.
+num1 = int(input("Enter the First Number: "))
+num2 = int(input("Enter the Second Number: "))
+print("The Sum of two numbers is :", num1 + num2)
+
+# take the radius of circle and find the perimeter and circumfere of the circle.
+
+r = float(input("Enter the radius of the circle: "))
+pi = 3.14
+crcm = 2 * pi * r
+print("Circumference of the cicle with radius ", r, "is :", crcm)
+
+# take 2 float number and print the average of both the numbers
+numf1 = float(input("Enter the First Number: "))
+numf2 = float(input("Enter the Second Number: "))
+avgf2 = (numf1 + numf2) / 2
+print("The Average of two numbers is :", avgf2)
+
+#print True or false based.
+num1 = int(input("Enter the First Number: "))
+num2 = int(input("Enter the Second Number: "))
+print(num1, " > ", num2, "(True / False) :", num1 > num2)
